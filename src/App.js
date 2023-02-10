0658774e68a2c1fc2077cb2e14528c87b184b3d8
Tom Fairclough <tom_fairclough@btinternet.com>
2023-02-10 15:07:31 +0000

    TF- Create Route Nav Bar

--- src/App.js
+++ src/App.js
@@ -27,9 +27,19 @@ export default class App extends React.Component {
       <Router>
         <>
           <h1>Naptser Social app</h1>
+
+          {/* Nav bar links to each React Route */}
+          <nav>
+            <Link to = "/">Log In</Link>  
+            <Link to = "/feed">Feed</Link>
+            <Link to = "/profile">Profile</Link>
+          </nav>
+
+          {/* Creating the React Paths to different pages */}
           <Route exact path="/" component = { () => <Login users={this.state.users} setUsers={this.setUsers}/>}/>
           <Route exact path = "/feed"/> 
           <Route exact path = "/profile"/> 
+
         </>
       </Router>
     )
