8845b907a46e94010916620cca1030689233e641
Nick <nick.langley@hotmail.com>
2023-02-10 16:35:30 +0000

    added ability to add dummy users from frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Login from './components/users/loginRegister';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Login from './components/users/loginRegister';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      users: []
+      users: [],
+      currentUser: {
+        firstName: 'test',
+        lastName: 'test',
+        userName: 'test',
+        password: 'test',
+        email: 'test',
+        location: 'test',
+        friends: ['test'],
+        img: 'test',
+        timestamps: 'test'
+      }
     };
   }
 
@@ -19,11 +36,24 @@ export default class App extends React.Component {
 
   render() {
     return(
-      <>
-        <h1>Social app</h1>
-        <Login  users={this.state.users}
-                    setUsers={this.setUsers}/>
-      </>
+      <Router>
+        <>
+          <h1>Naptser Social app</h1>
+
+          {/* Nav bar links to each React Route */}
+          <nav>
+            <Link to = "/">Log In</Link>  
+            <Link to = "/feed">Feed</Link>
+            <Link to = "/profile">Profile</Link>
+          </nav>
+
+          {/* Creating the React Paths to different pages */}
+          <Route exact path="/" component = { () => <Login users={this.state.users} setUsers={this.setUsers}/>}/>
+          <Route exact path = "/feed"/> 
+          <Route exact path = "/profile"/> 
+
+        </>
+      </Router>
     )
   }
 } 
