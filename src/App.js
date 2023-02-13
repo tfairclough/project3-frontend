62ae70ecced11d3db4ea088e26d8a0ce2ecd330c
3ddy1985 <102239616+3ddy1985@users.noreply.github.com>
2023-02-13 12:17:14 +0000

    Nick update current user (#12)
    * NL - git setup test
    
    * NL - initial setup for react inc.. app.js, api config files, login component and tested the connection to backend by pulling all users. this will need to be removed as we shouldn't be caling all users here (was just for testing)
    
    * added a new api call to create a new user as a test so everyone could understand how to send data back to the database
    
    * Edited the login api to the register to create a new user
    
    * Edited the dummy user
    
    * resolve conflicts
    
    * added ability to add dummy users from frontend
    
    * I fixed the create dummy users issue. The bug was caused by the router setup on the app page. NB the exact path is only needed when you are using link to '/' only as this is used in all links
    
    * Created a login page that can switch between login and register. made some minor changes to app.js to allow logout, token save and moved the login route out of the main router. I also created a version of app and login as functions to use hooks. might be worth having a look as does seem to be the ideal way of coding react. I have added some css just to the login page
    
    * Added functionality to update currnt user state on login. there is also more css changes to login

--- src/App.js
+++ src/App.js
@@ -20,15 +20,16 @@ export default class App extends React.Component {
     this.state = {
       token: getToken(),
       currentUser: {
-        firstName: 'test',
-        lastName: 'test',
-        userName: 'test',
-        password: 'test',
-        email: 'test',
-        location: 'test',
-        friends: ['test'],
-        img: 'test',
-        timestamps: 'test'
+        firstName: '',
+        lastName: '',
+        userName: '',
+        // password: '',
+        // email: '',
+        // location: '',
+        friends: [''],
+        // posts: [''],
+        // img: '',
+        // timestamps: ''
       }
     };
   }
@@ -51,6 +52,13 @@ export default class App extends React.Component {
     })
   }
 
+<<<<<<< HEAD
+  setCurrentUser = (currentUser) => {
+    this.setState({ currentUser })
+  }
+
+=======
+>>>>>>> 2edc84f92406297f46b36b9732c0ea5286ac78dc
   // Saves token to local storage
   saveToken = (userToken) => {
     localStorage.setItem('token', userToken);
@@ -64,7 +72,12 @@ export default class App extends React.Component {
 
     // Checks if a token exists if not the login page is loaded
     if (!token) {
+<<<<<<< HEAD
+      return <Login setToken={this.saveToken} 
+      setCurrentUser={this.setCurrentUser}/>;
+=======
       return <Login setToken={this.saveToken} />;
+>>>>>>> 2edc84f92406297f46b36b9732c0ea5286ac78dc
     } else {
 
     return(
