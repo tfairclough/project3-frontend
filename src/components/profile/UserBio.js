eb47c50f730661198ec12157862af87304b4758e
Tom Fairclough <tom_fairclough@btinternet.com>
2023-02-14 13:40:26 +0000

    TF - Add Edit Icon to UserBio

--- src/components/profile/UserBio.js
+++ src/components/profile/UserBio.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import profilePic from '../../assets/profilePic.jpg'
+import EditIcon from '../../assets/pencil_icon.png'
 
 
 const UserBio = (props) => {
   return (
     <div>
       <img src={profilePic}/>
+      <img className='pencil-icon' src={EditIcon} onClick={props.editCountryDetails} width={50}/>
       <p>{props.currentUser.firstName}</p>
+      <p>{props.currentUser.lastName}</p>
+      <p>{props.currentUser.email}</p>
     </div>
   )
 }
