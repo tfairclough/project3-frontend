7b2c2d9a4a85fded0dad8f3dea2de37f34b8fdd8
tfairclough <118983625+tfairclough@users.noreply.github.com>
2023-02-16 19:27:45 +0000

    Tf removing use effect and fixing posts rendering/re-rendering (#30)
    * TF-Refcatored UseEffect
    
    * TF- Fix Post Edit Body Load & Conditional Rendering

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -3,36 +3,39 @@ import { findMyPosts, findPosts } from "../users/api";
 import Post from "../posts/Post";
 import CreatePost from "../posts/CreatePost"
 
-const Feed = ( {currentUser, profilePage} ) => {
+const Feed = (props) => {
   const [posts, setPosts] = useState([]);
+  
 
-  useEffect(() => {
-    if (currentUser.id) {
-      profilePage ? findMyPosts(currentUser.id)
-        .then((response) => {
-          setPosts(response.data.posts);
-        })
-        .catch((error) => {
-          console.error("Error fetching posts:", error);
-        })     
-      
-      : findPosts()
-        .then((response) => {
-          setPosts(response.data.posts.map((post) => post._id));
-        })
-        .catch((error) => {
-          console.error("Error fetching posts:", error);
-        });
-      }
-  }, []);
+  if (props.currentUser.id) {
+    props.profilePage ? findMyPosts(props.currentUser.id)
+      .then((response) => {
+        setPosts(response.data.posts);
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+      })     
+    
+    : findPosts()
+      .then((response) => {
+        setPosts(response.data.posts.map((post) => post._id));
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+      });
+    }
 
   return (
     <div>
       <h1>My Blog Posts</h1>
       {posts.map((postId) => (
-        <Post key={postId} postId={postId} />                
+        <Post key={postId} 
+              postId={postId}
+              currentUser={props.currentUser} 
+              updateCurrentUserFromDatabase={props.updateCurrentUserFromDatabase}
+              profilePage={props.profilePage}/>                
       ))}
-      {profilePage && <CreatePost currentUser={currentUser}/>}      
+      {props.profilePage && <CreatePost currentUser={props.currentUser}/>}      
 
     </div>
   );
