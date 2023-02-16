5e3559f88eb472889196e9201f22c6c93b9d7583
Ma-Add Al-Jabouri <meljaboury@outlook.com>
2023-02-16 11:51:49 +0000

    fixed merge errors

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from "react";
 import { findPosts } from "../users/api";
 import Post from "../posts/Post";
 import CreatePost from "../posts/CreatePost"
-<<<<<<< HEAD
 const Feed = ( {currentUser} ) => {
-=======
-const Feed = () => {
->>>>>>> 6e394ff571340488c6f599c6f2c09ef3f77d7ffc
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {    
@@ -25,11 +21,7 @@ const Feed = () => {
       {posts.map((post) => (
         <Post key={post._id} post={post} />                
       ))}
-<<<<<<< HEAD
       <CreatePost currentUser={currentUser}/>      
-=======
-      <CreatePost/>
->>>>>>> 6e394ff571340488c6f599c6f2c09ef3f77d7ffc
     </div>
   );
 };
--- src/components/posts/CreatePost.js
+++ src/components/posts/CreatePost.js
@@ -2,11 +2,7 @@ import { useState } from 'react';
 import { addPost } from "../users/api";
 
 // Define the CreatePost component
-<<<<<<< HEAD
 const CreatePost = ( {currentUser} ) => {
-=======
-const CreatePost = () => {
->>>>>>> 6e394ff571340488c6f599c6f2c09ef3f77d7ffc
   // Define the postContent state with the useState hook, and initialize it to an empty string
   const [postContent, setPostContent] = useState('');
   // Define the showForm state with the useState hook, and initialize it to false
@@ -16,12 +12,7 @@ const CreatePost = () => {
   const handlePostSubmit = (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
     // Call the addPost function from the API module, passing in the postContent state
-<<<<<<< HEAD
-    console.log(currentUser.id)
     addPost(currentUser.id, postContent)
-=======
-    addPost(postContent)
->>>>>>> 6e394ff571340488c6f599c6f2c09ef3f77d7ffc
       .then((response) => {
         console.log('Post created successfully:', response.data);        
         setShowForm(false); // Hide the form after the post is created        
