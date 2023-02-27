859fee00a9b92f6cbd56b4ec521a7840624dbdbf
Nick <nick.langley@hotmail.com>
2023-02-27 13:03:11 +0000

    deployment changes #

--- src/components/users/loginRegister.js
+++ src/components/users/loginRegister.js
@@ -22,7 +22,7 @@ export default class Login extends React.Component {
       location: '',
       isLoginForm: true,
       loginErrorMessage: '',
-      registerErrorMessage: [],
+      registerErrorMessage: '',
       showPassword: false,
     };
   } 
